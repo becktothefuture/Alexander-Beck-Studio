81cae04eed461ab6635ccad475a785344c14a3ac
becktothefuture <45033529+becktothefuture@users.noreply.github.com>
2026-07-11 22:27:12 +0100

    style(button-bar): reduce max width

--- react-app/app/src/lib/buttonBarControls.js
+++ react-app/app/src/lib/buttonBarControls.js
@@ -2,7 +2,7 @@ export const BUTTON_BAR_DEFAULTS = Object.freeze({
   buttonBarHeightPx: 78,
   buttonBarInsetPx: 10,
   buttonBarWidthPx: 2400,
-  buttonBarMaxWidthPx: 1280,
+  buttonBarMaxWidthPx: 1100,
   buttonBarGapPx: 16,
   buttonBarButtonHeightPx: 48,
   buttonBarButtonPaddingXPx: 16,
